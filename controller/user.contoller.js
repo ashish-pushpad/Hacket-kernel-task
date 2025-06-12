2291dad25132fb6e1c2d94504275c47c8b26236a
ashish-pushpad <ashishpushpad001@gmail.com>
2025-06-12 17:36:32 +0530

    add email and mobile verification

--- controller/user.contoller.js
+++ controller/user.contoller.js
@@ -11,16 +11,26 @@ console.log("this is save")
       return  res.status(400).json({"message":"All credential are require"});
     }
 
+    const isMobileValid = /^[6-9]\d{9}$/.test(mobile)
+    const isMailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    if(isMailValid){
+     return res.status(400).send("<h1>Invalid Mail</h1>")
+    }
+    if(isMobileValid){
+      return res.status(400).send("<h1>invalid Mobile no</h1>")
+    }
+
     const newUser = await User.query().insert({ name, email, mobile });
 
     console.log(newUser);
     if(!newUser){
-        return res.status(500).json({"message":"Internal server error " });
+        return res.status(500).send("<h1>Internal server error</h1> ");
     }
 
-    res.status(201).json({
-      "message": "User created successfully",
-    });
+    res.status(201).send(
+      "<h1>User created successfully </h1>",
+    );
   } catch (error) {
     console.log(error)
     res.status(500).json({ 'message':'somthign went wrong ' });
